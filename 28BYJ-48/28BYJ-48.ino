22378da52fadd02e09a34e5de59beda15e9f0f05
Tharaka Rupasinghe <tharaka.drive@gmail.com>
2025-01-30 15:23:29 +0530

    Create : ino file

---
+++ 28BYJ-48/28BYJ-48.ino
@@ -0,0 +1,23 @@
+// --- 28BYJ-48 Motor Control --- //
+
+// -- Pin Configuration -- //
+
+/*
+ULN2003 Driver      Arduino
+IN1                 8
+IN2                 9
+IN3                 10
+IN4                 11
+GND                 GND
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
